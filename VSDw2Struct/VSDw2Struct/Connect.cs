f5f6988e94b8439cbee5481c141977e07d7b2332
Benjamin Kemner <benjamin.kemner@gmx.de>
2011-08-09 21:04:21 +0200

    init

---
+++ VSDw2Struct/VSDw2Struct/Connect.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnvDTE;
+using EnvDTE80;
+using Extensibility;
+
+namespace Dw2Struct
+{
+    public class Connect : IDTExtensibility2, IDTCommandTarget
+    {
+        private DTE2 application;
+        private AddIn addInInstance;
+        private PBHelper pb;
+
+        public Connect()
+        {
+        }
+
+        public void Exec(string CmdName, vsCommandExecOption ExecuteOption, ref object VariantIn, ref object VariantOut, ref bool Handled)
+        {
+            // if the menu item "Generate Structure" is called
+            if(CmdName == "Dw2Struct.Connect.Dw2Struct")
+            {
+                if(application.SelectedItems.Count == 0) return;
+
+                if (pb != null)
+                {
+                    pb.GenerateStructure(application.SelectedItems.Item(1));
+                }
+            }
+            // if the menu item "Generate NonVisualObject" is called
+            else if (CmdName == "Dw2Struct.Connect.Dw2Nv")
+            {
+                if (application.SelectedItems.Count == 0) return;
+
+                if (pb != null)
+                {
+                    pb.GenerateNonVisualObject(application.SelectedItems.Item(1));
+                }
+            }
+                
+        }
+
+        public void QueryStatus(string CmdName, vsCommandStatusTextWanted NeededText, ref vsCommandStatus StatusOption, ref object CommandText)
+        {
+            if (NeededText == vsCommandStatusTextWanted.vsCommandStatusTextWantedNone)
+            {
+                if (CmdName == "Dw2Struct.Connect.Dw2Struct" || CmdName == "Dw2Struct.Connect.Dw2Nv")
+                {
+                    if (application.SelectedItems.Count == 0)
+                    {
+                        StatusOption = vsCommandStatus.vsCommandStatusInvisible;
+                        return;
+                    }
+
+                    // it's only for datawindows
+                    if (pb.IsDatawindow(application.SelectedItems.Item(1)))
+                    {
+                        StatusOption = (vsCommandStatus)vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled;
+                        return;
+                    }
+                    else
+                    {
+                        StatusOption = vsCommandStatus.vsCommandStatusInvisible;
+                        return;
+                    }
+                }
+
+            }
+        }
+
+        public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+            pb = new PBHelper();
+            application = (DTE2)Application;
+            addInInstance = (AddIn)AddInInst;
+            if (ConnectMode == ext_ConnectMode.ext_cm_UISetup)
+            {
+                object[] contextGUIDS = new object[] { };
+                Commands2 commands = (Commands2)application.Commands;
+                
+                // get item bar
+                Microsoft.VisualStudio.CommandBars.CommandBar itemCommandBar = ((Microsoft.VisualStudio.CommandBars.CommandBars)application.CommandBars)["Item"];
+
+                try
+                {
+                    // add me commands
+                    Command command = commands.AddNamedCommand2(addInInstance, "Dw2Struct", "Generate Structure", "Generates a structure of the columns in the datawindow", true, 59, ref contextGUIDS, (int)vsCommandStatus.vsCommandStatusSupported + (int)vsCommandStatus.vsCommandStatusEnabled, (int)vsCommandStyle.vsCommandStylePictAndText, vsCommandControlType.vsCommandControlTypeButton);
+                    Command command2 = commands.AddNamedCommand2(addInInstance, "Dw2Nv", "Generate NonVisualObject", "Generates an NonVisualObject of the columns in the datawindow", true, 59, ref contextGUIDS, (int)vsCommandStatus.vsCommandStatusSupported + (int)vsCommandStatus.vsCommandStatusEnabled, (int)vsCommandStyle.vsCommandStylePictAndText, vsCommandControlType.vsCommandControlTypeButton);
+                                        
+                    if ((command != null))
+                    {
+                        command.AddControl(itemCommandBar, itemCommandBar.Controls.Count);                        
+                    }
+
+                    if ((command2 != null))
+                    {
+                        command2.AddControl(itemCommandBar, itemCommandBar.Controls.Count);
+                    }
+                }
+                catch (System.ArgumentException)
+                {
+                }
+            }
+        }
+
+        #region some implementations it didn't need
+
+        public void OnAddInsUpdate(ref Array custom)
+        {
+
+        }
+
+        public void OnBeginShutdown(ref Array custom)
+        {
+
+        }
+
+        public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+        {
+            
+        }
+
+        public void OnStartupComplete(ref Array custom)
+        {
+
+        }
+
+        #endregion some implementations it didn't need
+    }
+}
---
+++ VSDw2Struct/VSDw2Struct/PBHelper.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnvDTE;
+using System.IO;
+using dw2structnv;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace Dw2Struct
+{
+    public class PBHelper
+    {
+        /// <summary>
+        /// retrieves the path of the selcected item
+        /// </summary>
+        /// <param name="item">selected item</param>
+        /// <returns>file path</returns>
+        private string GetSelectedFileName(SelectedItem item)
+        {
+            if (item != null)
+            {
+                if (item.ProjectItem != null)
+                {
+                    return item.ProjectItem.get_FileNames(1);
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// checks the extension and indicates if it is a datawindow
+        /// </summary>
+        /// <param name="item">selected item</param>
+        /// <returns>flag</returns>
+        public bool IsDatawindow(SelectedItem item)
+        {           
+            string file = GetSelectedFileName(item);
+
+            if (!String.IsNullOrEmpty(file))
+            {
+                if (file.Substring(file.Length - 4, 4).ToLower().Equals(".srd"))
+                    return true;
+            }
+            
+            return false;
+        }
+
+        /// <summary>
+        /// generates a structure from the datawindow
+        /// </summary>
+        /// <param name="item"></param>
+        public void GenerateStructure(SelectedItem item)
+        {
+            string file = GetSelectedFileName(item);            
+
+            if (!String.IsNullOrEmpty(file))
+            {
+                bool fileNew = false;
+                string syntax = null;
+                string structure = null;                
+                string structName = Regex.Replace(file.Substring(file.LastIndexOf("\\") +1).Replace(".srd", ""),"^d_", "s_");
+                string structFile = file.Substring(0, file.LastIndexOf("\\") +1) + structName + ".srs";
+                StreamReader reader = null;
+                StreamWriter writer = null;
+                n_dw2struct dw2struct = new n_dw2struct();
+
+                reader = new StreamReader(new FileStream(file, FileMode.Open));
+                syntax = reader.ReadToEnd();
+                reader.Close();
+
+                structure = dw2struct.GenerateStructure(syntax, structName);
+
+                if (File.Exists(structFile))
+                {
+                    if (MessageBox.Show("The File " + structFile + " does already exist. Do you want to override it?", "Override", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+                        return;
+                    else
+                        File.Delete(structFile);
+                }
+                else
+                {
+                    fileNew = true;
+                }
+
+
+                writer = new StreamWriter(new FileStream(structFile, FileMode.CreateNew));
+                writer.Write(structure);
+                writer.Flush();
+                writer.Close();
+
+                if(fileNew)
+                    AddToPbl(structFile, structName + ".srs");
+            }
+        }
+
+        /// <summary>
+        /// generates an nvo from the datawindow
+        /// </summary>
+        /// <param name="item"></param>
+        public void GenerateNonVisualObject(SelectedItem item)
+        {
+            string file = GetSelectedFileName(item);  
+
+            if (!String.IsNullOrEmpty(file))
+            {
+                bool fileNew = false;
+                string syntax = null;
+                string nonv = null;
+                string nonvName = Regex.Replace(file.Substring(file.LastIndexOf("\\") + 1).Replace(".srd", ""), "^d_", "n_");
+                string nonvFile = file.Substring(0, file.LastIndexOf("\\") + 1) + nonvName + ".sru";
+                StreamReader reader = null;
+                StreamWriter writer = null;
+                n_dw2struct dw2struct = new n_dw2struct();
+
+                reader = new StreamReader(new FileStream(file, FileMode.Open));
+                syntax = reader.ReadToEnd();
+                reader.Close();
+
+                nonv = dw2struct.GenerateNonvisual(syntax, nonvName);
+
+                if (File.Exists(nonvFile))
+                {
+                    if (MessageBox.Show("The File " + nonvFile + " does already exist. Do you want to override it?", "Override", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+                        return;
+                    else
+                        File.Delete(nonvFile);
+                }
+                else
+                {
+                    fileNew = true;
+                }
+
+
+                writer = new StreamWriter(new FileStream(nonvFile, FileMode.CreateNew));
+                writer.Write(nonv);
+                writer.Flush();
+                writer.Close();
+
+                if(fileNew)
+                    AddToPbl(nonvFile, nonvName + ".sru");
+            }
+        }
+
+        /// <summary>
+        /// adds an new object to the (virtual) pbl
+        /// </summary>
+        /// <param name="objectfile"></param>
+        /// <param name="name"></param>
+        public void AddToPbl(string objectfile, string name)
+        {
+            string file = objectfile.Substring(0,objectfile.LastIndexOf(".pbl") + 4);
+
+            string[] files = Directory.GetFiles(file, "*.pblx").ToArray();
+
+            if (files.Length == 1)
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+
+                xmlDoc.Load(files[0]);
+
+                XmlNodeList nodes = xmlDoc.GetElementsByTagName("Project");
+                nodes[0].InnerXml += "<ItemGroup><Compile Include=\"" + name  + "\">" +
+                                    "<SubType>Code</SubType>" +
+                                    "</Compile></ItemGroup>";
+
+                xmlDoc.Save(files[0]);
+            }
+            
+        }
+
+    }
+}
